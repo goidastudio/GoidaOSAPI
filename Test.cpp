e8e1d70cb5aabdd39af14891262369a88dd98c48
goidastudio <goidastudiooffc@gmail.com>
2025-05-21 21:10:26 +0300

    Create Test.cpp
    Test app with first OS API functions

---
+++ Test.cpp
@@ -0,0 +1,25 @@
+#include<gosapi>
+
+int main(int argc, char *argv[])
+{
+	SetApiV(1, 0);
+	//SetApiV(1, 0); - set the api version to 1.0
+	//SetApiV(1, 1); -set api version to 1.1
+	//last api v - 1.1
+	//...
+	cnsl("Hello, World");
+	//print the "Hello, World"
+	//...
+	fs::wrf("test.txt", "");//write file
+	cnsl(fs::rf("test.txt"));//read file
+	fs::delf("test.txt");//delete file
+	fs::crdir("dir/");//create dir
+	fs::deldir("dir/");//delete dir
+	if(fs::direx("dir/")){//directory exists
+		cnsl("dir not deleted");
+	}
+	else{
+		cnsl("dir deleted");
+	}
+	//fs::fex(path) - file exists
+}
